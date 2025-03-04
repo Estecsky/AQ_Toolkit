35a443b91c8523a45e1ee1dbb79e9380068134d6
Estecsky <apsurtec@gmail.com>
2025-03-04 17:27:20 +0800

    add Auto_Xray_Shading property

--- AQ_property.py
+++ AQ_property.py
@@ -15,3 +15,8 @@ class AQ_CusProperty(bpy.types.PropertyGroup):
         min=1,
         max=10,
     )  # type: ignore
+    Auto_Xray_Shading: BoolProperty(
+        name="Auto Xray Shading",
+        description="保留面后自动打开透视模式",
+        default=False,
+    )  # type: ignore
